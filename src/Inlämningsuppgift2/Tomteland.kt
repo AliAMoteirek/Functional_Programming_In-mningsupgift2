ba90e65ea5531428cdf15035672f447aca0d1d35
Ali Moteirek <mtairek20@gmail.com>
2022-01-09 12:01:41 +0100

    Reading data from a text file instead of hard coding the data

--- src/Inlämningsuppgift2/Tomteland.kt
+++ src/Inlämningsuppgift2/Tomteland.kt
@@ -1,16 +1,33 @@
 package Inlämningsuppgift2
 
+import java.io.File
+import java.io.FileNotFoundException
+import java.lang.Exception
+
 class Tomteland {
-    private val mapOfTomte = mapOf(
-        "Tomten" to listOf("Glader", "Butter"),
-        "Glader" to listOf("Tröger", "Trötter", "Blyger"),
-        "Butter" to listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven"),
-        "Trötter" to listOf("Skumtomten"), "Skumtomten" to listOf("Dammråttan"),
-        "Räven" to listOf("Gråsuggan", "Myran"), "Myran" to listOf("Bladlusen")
-    )
+    private val mapList = mutableMapOf<String, List<String>>()
+
+    private fun constructMapOfTomtar() {
+        try {
+            File("file.txt").useLines { lines ->
+                lines.groupBy {
+                    val list = it.split(", ")
+                    mapList.put(list[0], list.subList(1, list.size))
+                }
+                //mapList.forEach { println(it) }
+            }
+        } catch (e: FileNotFoundException) {
+            println("The text file is not found")
+            e.printStackTrace()
+        } catch (e: Exception) {
+            println("Something went wrong")
+            e.printStackTrace()
+        }
+    }
 
     fun getUnderlings(currentName: String, res: MutableList<String>): List<String> {
-        mapOfTomte.forEach { (key, value) ->
+        constructMapOfTomtar()
+        mapList.forEach { (key, value) ->
             value.filter { key == currentName }.forEach {
                 res += it
                 getUnderlings(it, res)
